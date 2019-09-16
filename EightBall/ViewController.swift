881cef5f9c3338946caa03abe5fe793e43635b0e
Ivelisse Mandato <mandatoi@bc.edu>
2019-09-16 18:09:00 -0400

    completed

--- EightBall/ViewController.swift
+++ EightBall/ViewController.swift
@@ -11,26 +11,6 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var resultLabel: UILabel!
-    var responses = ["It is certain",
-                     "It is decidedly so",
-                     "Without a doubt",
-                     "Yes, definitely",
-                     "You may rely on it",
-                     "As I see it, yes",
-                     "Most likely",
-                     "Outlook good",
-                     "Yes",
-                     "Signs point to yes",
-                     "Reply hazy try again",
-                     "Ask again later",
-                     "Better not tell you now",
-                     "Cannot predict now",
-                     "Concentrate and ask again",
-                     "Don't count on it",
-                     "My reply is no",
-                     "My sources say no",
-                     "Outlook not so good",
-                     "Very doubtful"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +19,27 @@ class ViewController: UIViewController {
     
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let responses = ["It is certain",
+                         "It is decidedly so",
+                         "Without a doubt",
+                         "Yes, definitely",
+                         "You may rely on it",
+                         "As I see it, yes",
+                         "Most likely",
+                         "Outlook good",
+                         "Yes",
+                         "Signs point to yes",
+                         "Reply hazy try again",
+                         "Ask again later",
+                         "Better not tell you now",
+                         "Cannot predict now",
+                         "Concentrate and ask again",
+                         "Don't count on it",
+                         "My reply is no",
+                         "My sources say no",
+                         "Outlook not so good",
+                         "Very doubtful"]
+        
         resultLabel.text = responses.randomElement()!
     }
     
